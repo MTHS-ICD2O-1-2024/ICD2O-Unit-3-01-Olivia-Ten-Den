67f87086c120f4dc66f12ced8dfdbbf3cc5f4331
default <olivia.ten-den@stu.ocsb.ca>
2025-03-28 16:27:45 +0000

    copied in

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Olivia Ten Den
+// Created on: Mar 20, 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateAreaOfTriangle() {
+  // input
+  const baseOfTriangle = parseFloat(
+    document.getElementById("base-of-triangle").value
+  )
+  const heightOfTriangle = parseFloat(
+    document.getElementById("height-of-triangle").value
+  )
+
+  // process
+  const areaOfTriangle = (baseOfTriangle * heightOfTriangle) / 2
+
+  // output
+  document.getElementById("area").innerHTML =
+    "The area is: " + areaOfTriangle + " cm²"
+}
